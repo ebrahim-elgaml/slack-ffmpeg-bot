279d069679f24c00311ad112e781de181245e893
Ebrahim <ebrahim.el.gaml@gmail.com>
2016-05-04 17:16:11 +0200

    remove tokens

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-var express = require('express'),
+var config = require("./config.js"),
+    express = require('express'),
     SlackBot = require('slackbots'),
     ffmpeg = require('fluent-ffmpeg'),
     fs = require('fs'),
     https = require('https');
 
-var testToken = "xoxp-39696235703-39688555638-40038383251-9dd23314ec";
+var testToken = config.slack_token;
 var app = express();
 var bot = new SlackBot({
-    token: 'xoxb-39686298435-0KLvBGLX1kuOp357BubSSzIN', // Add a bot https://my.slack.com/services/new/bot and put the token
+    token: config.bot_token, // Add a bot https://my.slack.com/services/new/bot and put the token
     name: 'lucy'
 });
 var params = {
@@ -83,13 +84,14 @@ function compressOnlineVideo(videoId, path, data){
   if(user == null){
     return;
   }
+  bot.postMessageToUser(user, "Fetching the video", params);
   var proc = new ffmpeg(path)
       .addOption('-c:v',  'libx264', '-profile:v', 'baseline', '-level', '3.0', '-b:v', '800k')
       .addOption('-g', 10, '-qmin', 10, '-qmax', 51, '-i_qfactor', 0.71, '-qcomp', 0.6, '-me_method', 'hex')
       .addOption('-subq', 5, '-r', 20/1 ,'-pix_fmt', 'yuv420p')
       .addOption('-c:a', 'libfdk_aac', '-ac', 2 ,'-ar', 44100)
       .on('progress', function(progress) {
-        bot.postMessageToUser(user, 'Processing: ' + progress.frames + ' frames done', params);
+        bot.postMessageToUser(user, "Do not worry still working for you : " + 'Processing: ' + progress.frames + ' frames  done', params);
       })
       .on('start', function(commandLine) {
         console.log("%s: Spawned FFmpeg with command: %s", commandLine);
